4efd4344e40459663e3a5d44124304a6f25f1f78
Raj <rajkaushikbhai.raval@dcmail.ca>
2024-08-01 18:04:39 -0400

    4th Commit of Assignment 4

--- Assignments/Assignment 4/Part-2/main.js
+++ Assignments/Assignment 4/Part-2/main.js
@@ -6,8 +6,6 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-
-/* Declaring the alternative text for each image file */
 const alts = {
     'pic1.jpg' : 'Closeup of a human eye',
     'pic2.jpg' : 'Rock that looks like a wave',
@@ -27,13 +25,7 @@ for (const image of images) {
       displayedImage.alt = e.target.alt;
     });
   }
-
   
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
